5fa8815693d70dd5c233b399f8771f61e4796926
WornWolf <thanakorn6661@gmail.com>
2024-11-09 22:45:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
-# ต่อจากนั้นรันคำสั่ง build ของคุณ
+
+# อัปเดตข้อมูลจาก package repository
+apt-get update
+
+# ติดตั้ง FFmpeg
+apt-get install -y ffmpeg
+
+# ติดตั้ง dependencies ที่ระบุใน requirements.txt
+python -m pip install --upgrade pip  # อัปเกรด pip ให้เป็นเวอร์ชันล่าสุด
 python -m pip install -r requirements.txt

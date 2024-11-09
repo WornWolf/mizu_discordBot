d60f01cae728228fd0067d854bd3da7ccf2f28bb
WornWolf <thanakorn6661@gmail.com>
2024-11-10 01:30:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
+
+# อัปเดตข้อมูลจาก package repository
+apt-get update
+
+# ติดตั้ง FFmpeg
+apt-get install -y ffmpeg
+
+# ติดตั้ง dependencies ที่ระบุใน requirements.txt
+python -m pip install --upgrade yt-dlp
+python -m pip install --upgrade pip  # อัปเกรด pip ให้เป็นเวอร์ชันล่าสุด
+python -m pip install -r requirements.txt
